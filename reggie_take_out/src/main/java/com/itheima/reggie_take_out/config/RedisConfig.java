659dee6b503d954122090ebec82bb4a222a48a8d
zyx <1813156367@qq.com>
2023-10-07 21:49:18 +0800

    redis环境的配置

---
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.itheima.reggie_take_out.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+        //默认的序列化方式是JDK的序列化方式，这里改为String的序列化方式
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
